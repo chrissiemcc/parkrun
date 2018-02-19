4b13776645d228d2a93472e10987da3a3619f2fc
Chrissie <chrissiemcc44@gmail.com>
2018-02-19 19:04:08 +0000

    Testing navigation drawer Improved action bar dynamics

--- app/src/main/java/com/parkrun/main/MainActivity.java
+++ app/src/main/java/com/parkrun/main/MainActivity.java
@@ -1,6 +1,9 @@
 package com.parkrun.main;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -14,9 +17,10 @@ import android.support.v7.widget.Toolbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
 {
     private ActionBarDrawerToggle actionBarDrawerToggle;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -33,16 +37,21 @@ public class MainActivity extends AppCompatActivity
         String display = "Welcome " + databaseUser.getDisplayName() + "!";
         textView.setText(display);
 
-        Toolbar toolbar = findViewById(R.id.nav_action);
+        Toolbar toolbar = findViewById(R.id.action_bar);
+        toolbar.setTitle("Home");
         setSupportActionBar(toolbar);
-        //Allows menu to override the action bar
+        //Initialises and sets the action bar
 
-        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        drawerLayout = findViewById(R.id.drawerLayout);
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         //Adds menu button to the action bar
+
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        //Initialises the navigation view and sets listener
 
         signOutButton.setOnClickListener(new View.OnClickListener()
         {
@@ -73,5 +82,48 @@ public class MainActivity extends AppCompatActivity
         startActivity(intent);
 
         finish();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item)
+    {
+        int id = item.getItemId();
+
+        Utilities utilities = new Utilities();
+
+        if(id == R.id.nav_account)
+        {
+            getSupportActionBar().setTitle("Account");
+            utilities.getAlertDialog("Account clicked!", "Account!", MainActivity.this);
+        }
+        else if(id == R.id.nav_settings)
+        {
+            getSupportActionBar().setTitle("Settings");
+            utilities.getAlertDialog("Settings clicked!", "Settings!", MainActivity.this);
+        }
+        else if(id == R.id.nav_logout)
+        {
+            signOut();
+        }
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        drawerLayout = findViewById(R.id.drawerLayout);
+
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else
+        {
+            super.onBackPressed();
+        }
     }
 }
